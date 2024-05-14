eefa70e7222dbd913251416284ff017dd93304cb
Enrique <ingenriquearce@gmail.com>
2024-05-14 16:47:32 -0400

    añadiendo un bosquejo de proyects.html para guardar ahi mis proyectos

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/proyects')
+def proyects():
+    return render_template('proyects.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
